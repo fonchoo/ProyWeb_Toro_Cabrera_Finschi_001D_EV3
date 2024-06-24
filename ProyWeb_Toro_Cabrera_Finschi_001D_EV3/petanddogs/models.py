92b12f6c0f69b802a99a52ee2a5449e6c379f4c9
Patricio Finschi <patofinschi@gmail.com>
2024-06-24 05:21:21 -0400

    Update models.py

--- ProyWeb_Toro_Cabrera_Finschi_001D_EV3/petanddogs/models.py
+++ ProyWeb_Toro_Cabrera_Finschi_001D_EV3/petanddogs/models.py
@@ -2,6 +2,28 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.utils import timezone
 
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name="Id de categoria")
+    nombreCategoria=models.CharField(max_length=50, blank=True, verbose_name="Nombre de Categoria")
+
+    def __str__(self):
+        return self.nombreCategoria
+
+
+class Producto(models.Model):
+    id_producto              = models.CharField(primary_key=True, max_length=10)
+    nombre           = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=5000)
+    imagen=models.ImageField(upload_to="imagenes", null=True, blank=True, verbose_name="Imagen")
+    precio=models.IntegerField(blank=True, null=True, verbose_name="Precio")
+    stock = models.IntegerField(verbose_name="Stock")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name="Categoría")
+
+    def __str__(self):
+        return self.nombre 
+
+
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
